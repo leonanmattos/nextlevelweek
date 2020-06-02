7fcbbad1dfca2c1a26e935e1eb8147e9771d61ce
Leonan Mattos <Leonan@MacBook-Pro-de-USER.local>
2020-06-02 11:44:42 -0300

    Remove node_modules

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuário');
-
-    response.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Daniel'
-    ]);
-});
-
+app.use(express.json());
+app.use(routes);
 app.listen(3333);
 
 
